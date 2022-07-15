389edd4fde74d80d5b4a8d5e0a98ea61bfccff09
Robin Dousse <dousse@adobe.com>
2022-07-15 13:58:21 +0100

    New keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -21,28 +21,40 @@ function G.setup_keymaps()
   vim.api.nvim_set_keymap("s", "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {expr = true})
   vim.api.nvim_set_keymap("n", "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {expr = true})
 
-  -- LAZYGIT
-  vim.api.nvim_set_keymap("n", "<leader>gg", ":LazyGit<CR>", opts)
-
-  local tel = require("telescope.builtin")
+  -- File
   vim.api.nvim_set_keymap("n", "<leader>ff", ":lua require('telescope.builtin').find_files()<CR>", opts)
   vim.api.nvim_set_keymap("n", "<leader>fb", ":lua require('telescope.builtin').buffers()<CR>", opts)
+  vim.api.nvim_set_keymap("n", "<leader>fe", ":lua require('telescope.builtin').live_grep()<CR>", opts)
+  vim.api.nvim_set_keymap("n", "<leader>fE", ":lua require('telescope.builtin').grep_string()<CR>", opts)
+
+  -- LAZYGIT
+  vim.api.nvim_set_keymap("n", "<leader>gg", ":LazyGit<CR>", opts)
+  -- Git 
+  local tel = require("telescope.builtin")
+  vim.api.nvim_set_keymap("n", "<leader>gs", ":lua require('telescope.builtin').git_status()<CR>", opts)
+  vim.api.nvim_set_keymap("n", "<leader>gc", ":lua require('telescope.builtin').git_commits()<CR>", opts)
+  vim.api.nvim_set_keymap("n", "<leader>gC", ":lua require('telescope.builtin').git_bcommits()<CR>", opts)
+  vim.api.nvim_set_keymap("n", "<leader>gb", ":lua require('telescope.builtin').git_branches()<CR>", opts)
 end
 
 local keymap_opts = { noremap=true, silent=true }
 function G.setup_buf_lsp_keymaps(bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  buf_set_keymap('n', '<leader>p', '<cmd>lua vim.lsp.buf.hover()<CR>', keymap_opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', keymap_opts)
-  buf_set_keymap('n', '<leader>rf', '<cmd>lua vim.lsp.buf.code_action()<CR>', keymap_opts)
-  buf_set_keymap('n', '<leader>R', '<cmd>lua vim.lsp.buf.references()<CR>', keymap_opts)
-  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', keymap_opts)
+  buf_set_keymap("n", "<leader>p", "<cmd>lua vim.lsp.buf.hover()<CR>", keymap_opts)
+  buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", keymap_opts)
+  buf_set_keymap("n", "<leader>rf", "<cmd>lua vim.lsp.buf.code_action()<CR>", keymap_opts)
+  buf_set_keymap("n", "<leader>R", "<cmd>lua vim.lsp.buf.references()<CR>", keymap_opts)
+  buf_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", keymap_opts)
   buf_set_keymap("n", "<leader>fs", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>", keymap_opts)
   buf_set_keymap("n", "<leader>fS", "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>", keymap_opts)
   buf_set_keymap("n", "<leader>fr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>", keymap_opts)
-  buf_set_keymap("n", "<leader>fq", ":lua require('telescope.builtin').diagnostics()<CR>", keymap_opts)
+  buf_set_keymap("n", "<leader>fq", "<cmd>lua require('telescope.builtin').diagnostics()<CR>", keymap_opts)
   buf_set_keymap("n", "<leader>jd", "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>", keymap_opts)
-  buf_set_keymap("n", "<leader>jt", "<cmd>lua vim.lsp.buf.type_definition('reuse_win': true)", keymap_opts)
+  buf_set_keymap("n", "<leader>jD", "<cmd>vs<CR><C-w><C-w>\
+                                     <cmd>lua require('telescope.builtin').lsp_definitions()<CR>", keymap_opts)
+  buf_set_keymap("n", "<leader>jt", "<cmd>lua require('telescope.builtin').lsp_type_definitions()<CR>()", keymap_opts)
+  buf_set_keymap("n", "<leader>jT", "<cmd>vs<CR><C-w><C-w>\
+                                     <cmd>lua require('telescope.builtin').lsp_type_definitions()<CR>", keymap_opts)
 end
 
 function G.setup_buf_lsp_format_keymaps(bufnr)
