74a770d50ffb436387eb0c7db628ce2dd360d406
Robin Dousse <rdousse63@gmail.com>
2022-11-16 00:03:29 +0100

    Completion update breaking change

--- lua/completion.lua
+++ lua/completion.lua
@@ -41,7 +41,7 @@ function G.setup_completion()
   })
 end
 
-G.cmp_capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+G.cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 
 return G
