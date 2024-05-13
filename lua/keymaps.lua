3460f5cf9a7bcdd8fa5457799da85ed4f4445650
Robin Dousse <rdousse63@gmail.com>
2024-05-13 23:40:20 +0200

    scroll doc cmp differently

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -101,8 +101,8 @@ G.completion_keymaps = {
               feedkey("<Plug>(vsnip-jump-prev)", "")
             end
           end, { "i", "s" }),
-      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
       ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
       ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
       ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
