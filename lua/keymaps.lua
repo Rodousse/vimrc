25aa4710ccaa6e6940cefdead0af6f8f7169db15
Robin Dousse <rdousse63@gmail.com>
2024-05-13 23:28:20 +0200

    use nvim function for keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,51 +3,51 @@ local G = {}
 function G.setup_keymaps()
   local opts = { noremap=true, silent=true }
 
-  vim.api.nvim_set_keymap("t", "<esc><esc>", "<C-\\><C-n>", {noremap = true})
-  vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>ClangdSwitchSourceHeader<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>ToggleTerm direction=float<CR>", opts)
+  vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", {noremap = true})
+  vim.keymap.set("n", "<leader>h", "<cmd>ClangdSwitchSourceHeader<CR>", opts)
+  vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm direction=float<CR>", opts)
 
   -- Use <Tab> and <S-Tab> to navigate through snippet placeholder
-  vim.api.nvim_set_keymap('i', "<Tab>", 'pumvisible()?"<C-n>":"<Tab>"', {noremap = true, expr = true})
-  vim.api.nvim_set_keymap('i', "<S-Tab>", 'pumvisible()?"<C-p>":"<S-Tab>"', {noremap = true, expr = true})
-  vim.api.nvim_set_keymap("i", "<Tab>", "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)'      : '<Tab>'", {expr = true})
-  vim.api.nvim_set_keymap("s", "<Tab>", "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)'      : '<Tab>'", {expr = true})
-  vim.api.nvim_set_keymap("i", "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {expr = true})
-  vim.api.nvim_set_keymap("s", "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {expr = true})
-  vim.api.nvim_set_keymap("n", "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {expr = true})
+  vim.keymap.set('i', "<Tab>", 'pumvisible()?"<C-n>":"<Tab>"', {noremap = true, expr = true})
+  vim.keymap.set('i', "<S-Tab>", 'pumvisible()?"<C-p>":"<S-Tab>"', {noremap = true, expr = true})
+  vim.keymap.set("i", "<Tab>", "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)'      : '<Tab>'", {expr = true})
+  vim.keymap.set("s", "<Tab>", "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)'      : '<Tab>'", {expr = true})
+  vim.keymap.set("i", "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {expr = true})
+  vim.keymap.set("s", "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {expr = true})
+  vim.keymap.set("n", "<S-Tab>", "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'", {expr = true})
 
   -- File
-  vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>fe", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>fE", "<cmd>lua require('telescope.builtin').grep_string()<CR>", opts)
+  vim.keymap.set("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
+  vim.keymap.set("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", opts)
+  vim.keymap.set("n", "<leader>fe", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
+  vim.keymap.set("n", "<leader>fE", "<cmd>lua require('telescope.builtin').grep_string()<CR>", opts)
 
   --  Commands
-  vim.api.nvim_set_keymap("n", "<leader>c", "<cmd>lua require('telescope.builtin').commands()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>C", "<cmd>lua require('telescope.builtin').command_history()<CR>", opts)
+  vim.keymap.set("n", "<leader>c", "<cmd>lua require('telescope.builtin').commands()<CR>", opts)
+  vim.keymap.set("n", "<leader>C", "<cmd>lua require('telescope.builtin').command_history()<CR>", opts)
 
   -- search
-  vim.api.nvim_set_keymap("n", "<leader>/", "<cmd>lua require('telescope.builtin').search_history()<CR>", opts)
+  vim.keymap.set("n", "<leader>/", "<cmd>lua require('telescope.builtin').search_history()<CR>", opts)
 
   -- Git
-  vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>lua require('telescope.builtin').git_status()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>gc", "<cmd>lua require('telescope.builtin').git_commits()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>gC", "<cmd>lua require('telescope.builtin').git_bcommits()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>lua require('telescope.builtin').git_branches()<CR>", opts)
+  vim.keymap.set("n", "<leader>gs", "<cmd>lua require('telescope.builtin').git_status()<CR>", opts)
+  vim.keymap.set("n", "<leader>gc", "<cmd>lua require('telescope.builtin').git_commits()<CR>", opts)
+  vim.keymap.set("n", "<leader>gC", "<cmd>lua require('telescope.builtin').git_bcommits()<CR>", opts)
+  vim.keymap.set("n", "<leader>gb", "<cmd>lua require('telescope.builtin').git_branches()<CR>", opts)
 
   -- Debug
-  vim.api.nvim_set_keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>dB", "<cmd>lua require'dap'.list_breakpoints()<CR><cmd>lua require('telescope.builtin').quickfix()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>lua require'dap'.continue()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>dr", "<cmd>lua require'dap'.restart()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>du", "<cmd>lua require'dap'.up()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>dU", "<cmd>lua require'dap'.up()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>dp", "<cmd>lua require'dapui'.eval()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>ds", "<cmd>lua require'dap'.step_over()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<leader>do", "<cmd>lua require'dap'.step_out()<CR>", opts)
+  vim.keymap.set("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
+  vim.keymap.set("n", "<leader>dB", "<cmd>lua require'dap'.list_breakpoints()<CR><cmd>lua require('telescope.builtin').quickfix()<CR>", opts)
+  vim.keymap.set("n", "<leader>dd", "<cmd>lua require'dap'.continue()<CR>", opts)
+  vim.keymap.set("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<CR>", opts)
+  vim.keymap.set("n", "<leader>dc", "<cmd>lua require'dap'.continue()<CR>", opts)
+  vim.keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.restart()<CR>", opts)
+  vim.keymap.set("n", "<leader>du", "<cmd>lua require'dap'.up()<CR>", opts)
+  vim.keymap.set("n", "<leader>dU", "<cmd>lua require'dap'.up()<CR>", opts)
+  vim.keymap.set("n", "<leader>dp", "<cmd>lua require'dapui'.eval()<CR>", opts)
+  vim.keymap.set("n", "<leader>ds", "<cmd>lua require'dap'.step_over()<CR>", opts)
+  vim.keymap.set("n", "<leader>di", "<cmd>lua require'dap'.step_into()<CR>", opts)
+  vim.keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<CR>", opts)
 end
 
 local keymap_opts = { noremap=true, silent=true }
