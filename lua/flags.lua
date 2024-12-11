4f1246733d7bb59aeffc8c1dfce5f5bd50d0cd38
Robin Dousse <rdousse63@gmail.com>
2024-12-12 00:23:08 +0100

    use new popup completeopt

--- lua/flags.lua
+++ lua/flags.lua
@@ -4,7 +4,7 @@ function G.set_global_variables()
   vim.g.mapleader = " "
 
   -- rhysd/vim-clang-format settings
-  vim.g['clang_format#detect_style_file'] = 1 -- Use .clangformat at the root of the project
+  vim.g['clang_format#detect_style_file'] = 1          -- Use .clangformat at the root of the project
   if vim.fn.has("win32") == 1 then
     vim.g["clang_format#command"] = "clang-format.exe" -- Fix embedded clang format set by msvc command prompt
   else
@@ -40,10 +40,9 @@ function G.set_options()
   -- Display line number relatively except the current line in absolute
   vim.opt.relativenumber = true
   vim.opt.number = true
-  vim.opt.completeopt = "menuone,noinsert,noselect,preview,noselect"  -- Set completeopt to have a better completion experience
+  vim.opt.completeopt = "menuone,noselect,popup" -- Set completeopt to have a better completion experience
   vim.opt.shortmess:append("c")
 end
-
 
 function G.set_events()
   -- Turn on syntax highligting
@@ -54,7 +53,7 @@ function G.set_color_theme()
 end
 
 function G.create_autocommands()
-  vim.api.nvim_create_autocmd({"VimEnter"}, {pattern = "*", callback = function() vim.cmd("ClangFormatAutoEnable") end})
+  vim.api.nvim_create_autocmd({ "VimEnter" }, { pattern = "*", callback = function() vim.cmd("ClangFormatAutoEnable") end })
 end
 
 return G
